c50eea42d29ef97148adae0a2280baaedfcd9fbb
Ruan <ruan_araujo20@hotmail.com>
2023-12-25 10:40:59 -0300

    Projeto Atualizado

---
+++ Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
+
+public class Category
+{
+    public Category()
+    {
+        
+    }
+    public Category(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+
+    [Key]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe o título")]
+    [MinLength(3, ErrorMessage = "A categoria deve ter pelo menos 3 Caracteres")]
+    [MaxLength(60, ErrorMessage = "A categoria deve ter no máximo 60 Caracteres")]
+    public string Title { get; set; } = string.Empty;
+}
